06f9529a2bea23efa2665b0f1eac8ca00b9bfca3
xt <1347406392@qq.com>
2021-04-12 07:03:37 +0000

    初始化仓库

---
+++ 10-6.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+a=2
+b=1
+
+if [[ $a -lt $b ]];then
+	echo $a
+else
+	echo $b
+fi
+
---
+++ 4-2.sh
@@ -0,0 +1,6 @@
+MyFirstLetters=ABC
+echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
+
+greeting='Hello        world!'
+
+echo $greeting" now with spaces: $greeting"
